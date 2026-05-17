01217523811b09bb4a0c157f70d4f0dae7346ece
Tasnuba Tarannum Taha <tasnuba556@gmail.com>
2026-05-17 23:12:49 +0600

    Initial calculator project

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+a=float(input("First Number:"))
+b=float(input("Second Number:"))
+o=input("choose one(+,-,*,/): ")
+if o == "+":
+  print("Answer:",a+b)
+elif o == "-":
+  print("Answer:",a-b)
+elif o == "*":
+  print("Answer:",a*b)
+elif o == "/":
+  print("Answer",a/b)
+else:
+  print("Envalid")
